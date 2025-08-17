356c4f55c5602506b2fe04207757206273965763
daichangya <daichangya@163.com>
2025-08-17 16:16:59 +0800

    添加 多语言

---
+++ docs/js/baidu-tongji.js
@@ -0,0 +1,7 @@
+    var _hmt = _hmt || [];
+    (function() {
+        var hm = document.createElement("script");
+        hm.src = "https://hm.baidu.com/hm.js?4bdc02239b6fe6f4e90583308d9ed3cd";
+        var s = document.getElementsByTagName("script")[0];
+        s.parentNode.insertBefore(hm, s);
+    })();
